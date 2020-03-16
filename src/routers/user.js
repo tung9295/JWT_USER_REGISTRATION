77556d6b61a8457ca409af0391254332c699f8c5
tung9295 <tung9295@gmail.com>
2020-03-16 20:54:39 +0900

    fix user login router

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/users', async (req, res) => {
+  //Create new user
   try {
     const user = new User(req.body);
     await user.save();
@@ -18,13 +19,13 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async(req, res) => {
   //Login a registered user
   try {
-      const { email, password } = req.body
-      const user = await User.findByCredentials(email, password)
-      if (!user) {
-          return res.status(401).send({error: 'Login failed! Check authentication credentials'})
-      }
-      const token = await user.generateAuthToken()
-      res.send({ user, token })
+    const { email, password } = req.body;
+    const user = await User.findByCredentials(email, password);
+    if (!user) {
+        return res.status(401).send({error: 'Login failed! Check authentication credentials'})
+    }
+    const token = await user.generateAuthToken()
+    res.send({ user, token })
   } catch (error) {
     res.status(400).send(error)
   }

24f3e40c96ade1bb2d7584cb80c114f65d1fa007
tung9295 <tung9295@gmail.com>
2020-03-17 18:26:00 +0900

    router users/ login by email without password

--- src/routers/user.js
+++ src/routers/user.js
@@ -16,19 +16,19 @@ router.post('/users', async (req, res) => {
   }
 });
 
-router.post('/users/login', async(req, res) => {
+router.post('/users/login', (req, res) => {
   //Login a registered user
-  try {
-    const { email, password } = req.body;
-    const user = await User.findByCredentials(email, password);
-    if (!user) {
-        return res.status(401).send({error: 'Login failed! Check authentication credentials'})
-    }
-    const token = await user.generateAuthToken()
-    res.send({ user, token })
-  } catch (error) {
-    res.status(400).send(error)
-  }
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(400).send('No user found')
+      }
+      const token = user.generateAuthToken();
+      res.status(200).send({ user, token})
+    })
+    .catch(error => {
+      res.status(400).send(email)
+    })
 });
 
 router.get('/users/me', auth, async(req, res) => {
